5724e66b6e14a469cb6cab10d203b16d663094e0
walter <walterrjr.86@gmail.com>
2022-12-28 14:51:38 -0300

    FINALIZADO

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,10 +2,55 @@
 
 namespace App\Models;
 
+use App\Scopes\YearScope;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+    protected $fillable = [
+        'user_id',
+        'title',
+        'body',
+        'date'
+    ];
+
+    protected $casts = [
+        'date' => 'datetime:d/m/Y',
+    ];
+
+    //INICIO EXEMPLOS USANDO LOCAL SCOPE
+    public function scopeLastWeek($query){
+        return $this->whereDate('date', '>=', now()->subDays(4))
+        ->whereDate('date', '<=', now()->subDays(1));
+    }
+    
+    public function scopeToday($query){
+        return $this->whereDate('date', '>=', now());
+    }
+    
+    public function scopeBetween($query, $firstDate, $lastDate){
+        $firstDate = Carbon::make($firstDate)->format('Y-m-d');
+        $lastDate = Carbon::make($lastDate)->format('Y-m-d');
+        return $this->whereDate('date', '>=', $firstDate)
+        ->whereDate('date', '<=', $lastDate);
+    }
+    //FIM EXEMPLOS USANDO LOCAL SCOPE
+
+    protected static function booted()
+    {
+        static::addGlobalScope(new YearScope);
+    }
+
+    /* public function getTitleAttribute($value) {
+        return strtoupper($value);
+    } */
+
+    public function setDateAttribute($value) {
+        $this->attributes['date'] = Carbon::make($value)->format('Y-m-d');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Models\Post;
 use App\Models\User;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Str;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,33 +16,131 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+
+//USANDO O observer PARA MONITORAR AÇÕES(EVENTOS) RELACIONADAS A REFERIDA MODEL, RETORNANDO O QUE FOR ESPECIFICADO NO OBSERVER
+Route::get('/observer', function() {
+    $user = User::first();
+    $post = Post::create([
+        'user_id' => $user->id,
+        'title' => 'New Title ' . Str::random(10),
+        'body' => Str::random(100),
+        'date' => now(),
+    ]);
+    return $post;
+});
+
+//USANDO O MÉTODO DE global scope, as funções são criadas na em arquivo próprio, ficando disponíveis a todas as models para uso
+Route::get('/global-scopes', function() {
+    /* $posts = Post::get();
+    return $posts; */
+});
+
+//USANDO O MÉTODO DE localScope, as funções são criadas na model, e reaproveitas no controller ou route
+Route::get('/local-scope', function() {
+    //$posts = Post::LastWeek()->get();
+   // $posts = Post::today()->get();
+    //$posts = Post::between('2022-12-22', '2022-12-25')->get();
+    //return $posts;
+});
+
+//EXEMPLO USANDO O MÉTODO MUTATOR PARA ALTERAR O DADO SALVO, ANTES QUE ELE CHEGUE NO BANCO
+Route::get('/mutators', function() {
+   /*  $user = User::first();
+    $post = Post::create([
+        'user_id' => $user->id,
+        'title' => 'Um novo título ' . Str::random(10),
+        'body' => Str::random(100),
+        'date' => now(),
+    ]);
+    return $post; */
+});
+
+//EXEMPLO USANDO O MÉTODO ACCESSOR CRIADO NA MODEL DE Post
+Route::get('/acessor', function() {
+    $post = Post::get();
+    return $post;
+});
+
+//EXEMPLO MÉTODO delete(), QUE APAGA UMA LINHA DO BANCO, APÓS ESTA SER ESPECIFICADA PELO find()
+Route::get('/delete', function() {
+    /* $post = Post::find(8);
+    $post->delete();
+
+    $posts = Post::get();
+    return $posts; */
+});
+
+//EXEMPLOS USANDO O updateOrCreate, que consegue fazer o update e save no mesmo método(mais produtivo)
+Route::get('update2', function () {
+    /* $post = Post::find(1);
+    $post->updateOrCreate(
+    [
+        'user_id' => 41,
+    ],
+    [
+        'title' => 'Title41ds atualizado com updateOrCreate',
+        'body' => 'Body41ds atualizado com updateOrCreate',
+        'date' => date('Y-m-d'),
+
+    ]);
+    $post = Post::get();
+    return $post; */
+});
+
+//EXEMPLOS USANDO MÉTODOS DE INSERÇÃO(MASS ASSIGNMENT(ATRIBUIÇÃO EM MASSA)) DO ELOQUENT-AULA-8-MODULO-3
+Route::get('/update', function () {
+    /* $post = Post::find(1);
+
+    $post->update([
+        'user_id' => 4,
+        'title' => 'Título atualizado',
+        'body' => 'Body atualizado',
+        'date' => date('Y-m-d')
+    ]);
+    $post = Post::get();
+    return $post; */
+});
+
+//EXEMPLOS USANDO MÉTODOS DE INSERÇÃO(MASS ASSIGNMENT) DO ELOQUENT-AULA-7-MODULO-3
+Route::get('/insert2', function () {
+     /* $post = Post::create([
+        'user_id' => 50,
+        'title' => 'Valor para o title',
+        'body' => 'Valor para o body',
+        'date' => date('Y-m-d'), 
+      ]); 
+      $posts = Post::get();
+    return $posts; */
+});
+
 //EXEMPLOS USANDO MÉTODOS DE INSERÇÃO(MANUAL) DO ELOQUENT-AULA-6-MODULO-3
-/* Route::get('/insert', function(Post $post) {
-    $post->user_id = 1;
+Route::get('/insert', function (Post $post) {
+    /*  $post->user_id = 1;
     $post->title = 'Primeiro Post';
     $post->body = 'Conteúdo do post';
     $post->date = date('Y-m-d');
     $post->save();
     $posts = Post::get();
-    return $posts;
-}); */
+    return $posts; */
+});
 
 //EXEMPLOS USANDO MÉTODOS DE ORDENAÇÃO DO ELOQUENT-AULA-5-MODULO-3
-/* Route::get('/orderby', function() {
-    $users = User::orderBy('name', 'desc')->get();
+Route::get('/orderby', function () {
+    /*  $users = User::orderBy('name', 'desc')->get();
 
-    return $users;
-}); */
+    return $users; */
+});
 
 //EXEMPLOS USANDO MÉTODOS DE PAGINAÇÃO DO ELOQUENT-AULA-4-MODULO-3
-/* Route::get('/pagination', function() {
-    $users = User::paginate(2);
+Route::get('/pagination', function () {
+    /* $users = User::paginate(2);
 
-    return $users;
-}); */
+    return $users; */
+});
 
-Route::get('/where', function(User $user) {
-    $filter = 'm';
+Route::get('/where', function (User $user) {
+    //$filter = 'm';
     //$users = $user->where('email', '=', 'lilly21@example.org')->first();
     //$users = $user->where('name', 'LIKE', "%{$filter}%")->get();
     //$users = $user->where('name', 'LIKE', "%{$filter}%")->orWhere('name', 'Lyda')->get();
@@ -55,7 +154,7 @@ Route::get('/where', function(User $user) {
 });
 
 //EXEMPLOS USANDO MÉTODOS DE CONSULTAS DO ELOQUENT-AULA-2-MODULO-3
-Route::get('/select', function() {
+Route::get('/select', function () {
     //$users = User::all();
     //$users = User::where('id', 1)->get();
     //$user = User::where('id', 10)->first();
